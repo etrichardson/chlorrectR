9bc75ad5b43895a2449a9c1faf63dcd817c04090
etrichardson <emy.t.richardson@gmail.com>
2026-06-26 09:39:19 -0700

    Add option to skip temperature correction

--- R/correct_fchl.R
+++ R/correct_fchl.R
@@ -2,75 +2,75 @@
 #'
 #' @description Requires temperature (celsius) and raw chlorophyll fluorescence data (ug/L). Applies Watras (2017) correction using 25 degrees celcius as the reference temperature and Richardson (2025) bias corrections. This should be applied to YSI 6600, YSI EXO, and Seabird WETStar data.
 #' 
-#' @param fchl Vector of numbers
-#' @param temp Vector of numbers
+#' @param fchl Vector of numbers.
 #' @param instr Instrument type must be one of the following: "EXO2", "FP", "WS", "6600"
-#' @param na_rm Whether `NA`s should be removed. Defaults to `TRUE`
-#'
+#' @param skip_tcorr Whether or not to skip the temperature correction step (for example, most USGS data already apply temperature corrections). Defaults to `FALSE`.
+#' @param temp Vector of numbers (Temperature in Celsius)
+#' @param na.rm Whether or not `NA`s should be removed. Defaults to `TRUE`.
+#' 
 #' @returns Corrected chlorophyll concentration data using 'temp' for the numbers provided to 'fchl'
 #' @export
 #' 
 #' @importFrom magrittr %>%
 #' 
 #' @examples
-#' # Temperature correction (for 6600, EXO2, or WS)
-#' 
+#' # TBD
 
-correct_fchl <- function(fchl = NULL, 
-  instr = NULL, temp = NULL, na_rm = TRUE){
+correct_fchl <- function(fchl = NULL, instr = NULL, 
+  skip_tcorr = FALSE, temp = NULL, na.rm = TRUE){
 
   # Error checks here
 
-if(is.null(fchl)==TRUE || is.numeric(fchl)!=TRUE || min(fchl, na.rm = TRUE) < 0)
+  if(is.null(fchl)==TRUE || is.numeric(fchl)!=TRUE || min(fchl, na.rm = TRUE) < 0)
     stop("fchl must be a vector of positive numbers")
 
-if(is.null(instr)==TRUE || is.character(instr)!=TRUE || !instr %in% c("EXO2", "FP", "WS", "6600")) 
+  if(is.null(instr)==TRUE || is.character(instr)!=TRUE || instr %in% c("EXO2", "FP", "WS", "6600") != TRUE) 
     stop('Instrument type must be one of the following: "EXO2", "FP", "WS", "6600"')
   
-if(is.logical(na_rm)!=TRUE){
-  warning("na_rm must be TRUE or FALSE. Setting to default (TRUE)")
-  na_rm <- TRUE }
+  if(is.logical(na.rm)!=TRUE){
+    warning("na.rm must be TRUE or FALSE. Setting to default (TRUE)")
+    na.rm <- TRUE }
 
-# if(na_rm == TRUE) {
-#   chlv2 <- fchl[!is.na(fchl)]
-#   tempv2 <- temp[!is.na(temp)]
-# } else {
-  chlv2 <- fchl
-   tempv2 <- temp
-# }
-
-if(instr!= "FP"){
-    if(is.null(tempv2)==TRUE || is.numeric(tempv2)!=TRUE || length(chlv2)!=length(tempv2))
-    stop("Temperature must be a vector of as many numbers as chlorophyll") }
+  if(instr != "FP"){
+    if(is.null(temp)==TRUE || is.numeric(temp)!=TRUE || length(fchl)!=length(temp))
+      stop("Temperature must be a vector of as many numbers as chlorophyll") 
+  }
   
   # Warnings here
 
-if(min(tempv2, na.rm = TRUE)<=10 | max(tempv2, na.rm = TRUE)>=45)
-  warning("Temperature must be in celsius")
+  if(is.logical(skip_tcorr)!=TRUE){
+    warning("skip_tcorr must be TRUE or FALSE. Setting to default (FALSE)")
+    skip_tcorr <- FALSE}
 
-if(any(chlv2[!is.na(chlv2)] >50))
-  warning("Chlorophyll fluorometer readings above 50 ug/L are less likely to be accurate")  
+  if(min(temp, na.rm = TRUE)<=10 | max(temp, na.rm = TRUE)>=45)
+    warning("Temperature must be in celsius")
+
+  if(any(fchl[!is.na(fchl)] >50))
+    warning("Chlorophyll fluorometer readings above 50 ug/L are less likely to be accurate")  
   
   # Actual correction code
+
+if(skip_tcorr == FALSE & instr != "FP"){
+  corr.temp_fchl <- fchl / (1 + (0.01 * (temp - 25)))}
+  else {corr.temp_fchl <- fchl}
+
   if(instr == "EXO2"){
-    corr.temp_fchl <- chlv2 / (1 + (0.01 * (tempv2 - 25)))
     corr.instr_fchl <- (1.29 * corr.temp_fchl) + 0.33
   } 
   else if (instr == "FP"){
-    corr.instr_fchl <- chlv2 %>% 
-    dplyr::mutate(chlv2 = dplyr::case_when(
-    chlv2 < 16 ~ 0.39*chlv2+0.33,
-    chlv2 >= 16 ~ 0.71*chlv2-4.66))  
+    corr.instr_fchl <- fchl %>% 
+    dplyr::mutate(fchl = dplyr::case_when(
+    fchl < 16 ~ 0.39*fchl+0.33,
+    fchl >= 16 ~ 0.71*fchl-4.66))  
   }
   else if (instr == "6600"){
-    corr.temp_fchl <- chlv2 / (1 + (0.01 * (tempv2 - 25)))
+    corr.instr_fchl <- corr.temp_fchl
   }
   else if (instr == "WS"){
-    corr.temp_fchl <- chlv2 / (1 + (0.01 * (tempv2 - 25)))
     corr.instr_fchl <- (0.72 * corr.temp_fchl) - 0.06
   }
 
-  if(na_rm == TRUE) {
+  if(na.rm == TRUE) {
 
     final_fchl <- corr.instr_fchl[!is.na(corr.instr_fchl)]
   } else {final_fchl <- corr.instr_fchl}
--- tests/testthat/test-correct_fchl.R
+++ tests/testthat/test-correct_fchl.R
@@ -1,30 +1,28 @@
 # Run all tests in this script:
 ## testthat::test_file(file.path("tests", "testthat", "test-correct_fchl.R"))
 
-
 #working: 
-correct_fchl(fchl = 3.67, instr = "EXO2", temp = 23.03, na_rm = TRUE)
-
+#correct_fchl(fchl = 3.67, instr = "EXO2", skip_tcorr = FALSE, temp = 23.03, na.rm = TRUE)
 
 # Error testing
 test_that("Errors work as desired", {
-  expect_error(correct_fchl(fchl = -2, instr = "EXO2", temp = 23.03, na_rm = TRUE))
-  expect_error(correct_fchl(fchl = "banana", instr = "EXO2", temp = 23.03, na_rm = TRUE))
-  expect_error(correct_fchl(fchl = NULL, instr = "EXO2", temp = 23.03, na_rm = TRUE)) 
+  expect_error(correct_fchl(fchl = -2, instr = "EXO2", skip_tcorr = FALSE, temp = 23.03, na.rm = TRUE))
+  expect_error(correct_fchl(fchl = "banana", instr = "EXO2", skip_tcorr = FALSE, temp = 23.03, na.rm = TRUE))
+  expect_error(correct_fchl(fchl = NULL, instr = "EXO2", skip_tcorr = FALSE, temp = 23.03, na.rm = TRUE)) 
   
-  expect_error(correct_fchl(fchl = 3.67, instr = NULL, temp = 23.03, na_rm = TRUE))
-  expect_error(correct_fchl(fchl = 3.67, instr = "banana", temp = 23.03, na_rm = TRUE))
+  expect_error(correct_fchl(fchl = 3.67, instr = NULL, skip_tcorr = FALSE, temp = 23.03, na.rm = TRUE))
+  expect_error(correct_fchl(fchl = 3.67, instr = "banana", skip_tcorr = FALSE, temp = 23.03, na.rm = TRUE))
 
-  expect_error(correct_fchl(fchl = 3.67, instr = "EXO2", temp = NULL, na_rm = TRUE))
-
+  expect_error(correct_fchl(fchl = 3.67, instr = "EXO2", skip_tcorr = FALSE, temp = NULL, na.rm = TRUE))
 })
 
 # Warning testing
 test_that("Warnings work as desired", {
-  expect_warning(correct_fchl(fchl = 3.67, instr = "EXO2", temp = 100, na_rm = TRUE))  
-  expect_warning(correct_fchl(fchl = 3.67, instr = "EXO2", temp = 1, na_rm = TRUE))
-  expect_warning(correct_fchl(fchl = 75, instr = "EXO2", temp = 25, na_rm = TRUE))
-  expect_warning(correct_fchl(fchl = 3.67, instr = "EXO2", temp = 23.03, na_rm = "banana"))
+  expect_warning(correct_fchl(fchl = 3.67, instr = "EXO2", skip_tcorr = FALSE, temp = 100, na.rm = TRUE))  
+  expect_warning(correct_fchl(fchl = 3.67, instr = "EXO2", skip_tcorr = FALSE, temp = 1, na.rm = TRUE))
+  expect_warning(correct_fchl(fchl = 75, instr = "EXO2", skip_tcorr = FALSE, temp = 25, na.rm = TRUE))
+  expect_warning(correct_fchl(fchl = 3.67, instr = "EXO2", skip_tcorr = FALSE, temp = 25, na.rm = "banana"))
+  expect_warning(correct_fchl(fchl = 3.67, instr = "EXO2", skip_tcorr = "banana", temp = 25, na.rm = TRUE))
 })
 
 # Message testing
@@ -41,7 +39,7 @@ test_that("Outputs are as expected", {
   test_temp<- c(23, 24, NA, 25, NA)
   
   # Use the function to calculate CV and calculate it by hand
-  fxn_correct_fchl <- correct_fchl(fchl = test_chl, instr = "WS", temp = test_temp, na_rm = FALSE)
+  fxn_correct_fchl <- correct_fchl(fchl = test_chl, instr = "WS", skip_tcorr = FALSE, temp = test_temp, na.rm = FALSE)
 
     test_corr.temp_fchl <- test_chl / (1 + (0.01 * (test_temp - 25)))
     test_correct_fchl  <- (0.72 * test_corr.temp_fchl) - 0.06
